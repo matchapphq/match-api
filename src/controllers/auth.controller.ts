ca5d9fb65103c94ea0a02a9d974bb8a3ce13e4b4
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-10-07 05:01:09 +0800

    feat: adding prettyJSON and login logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,9 @@
-import { createFactory } from "hono/factory";
+import { password } from "bun";
 import { validator } from "hono/validator";
-import { RegisterRequestSchema } from "../utils/auth.valid";
+import { createFactory } from "hono/factory";
 import type { userRegisterData } from "../utils/userData";
 import UserRepository from "../repository/user.repository";
+import { RegisterRequestSchema, LoginRequestSchema } from "../utils/auth.valid";
 
 class AuthController {
     private readonly factory = createFactory();
@@ -10,6 +11,28 @@ class AuthController {
 
     readonly register = this.factory.createHandlers(validator("json", (value, ctx) => {
         const parsed = RegisterRequestSchema.safeParse(value);
+        if (!parsed.success) {
+            return ctx.json({error: "Invalid request body", details: parsed.error}, 401);
+        }
+        return parsed.data;
+    }), async (ctx) => {
+        const body = ctx.req.valid("json");
+        if (!body) {
+            return ctx.json({ msg: "Invalid Request body" }, 401);
+        }
+        const userRequest: userRegisterData = {
+            ...body,
+        };
+        console.log(userRequest);
+        const user = await this.userRepository.createUser(userRequest);
+        if (!user) {
+            return ctx.json({ msg: "User not created" }, 401)
+        }
+        return ctx.json({msg: "User created", data: user});
+    })
+
+    readonly login = this.factory.createHandlers(validator('json', (value, ctx) => {
+        const parsed = LoginRequestSchema.safeParse(value);
         if (!parsed.success) {
             return ctx.json({error: "Invalid request body", details: parsed.error})
         }
@@ -19,16 +42,20 @@ class AuthController {
         if (!body) {
             return ctx.json({ msg: "Invalid Request body" }, 401)
         }
-        const userRequest: userRegisterData = {
-            ...body,
-        }
-        console.log(userRequest);
-        const user = await this.userRepository.createUser(userRequest);
+        const user = await this.userRepository.getUserByEmail(body.email);
         if (!user) {
-            return ctx.json({ msg: "User not created" }, 401)
+            return ctx.json({ msg: "User not found" }, 401)
         }
-        return ctx.json({msg: "User created", data: user});
-    })
+        const passwordMatch = await password.verify(body.password, user.password_hash);
+        if (!passwordMatch) {
+            return ctx.json({ msg: "Invalid password" }, 401)
+        }
+        return ctx.json({msg: "User logged in"});
+    });
+
+    readonly logout = this.factory.createHandlers(async (ctx) => {
+        return ctx.json({msg: "User logged out"});
+    });
 }
 
 export default AuthController;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
 import AuthService from "./services/auth.service";
 import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
 
 const app = new Hono();
 
 const authRouter = new AuthService();
 
 app.use(logger());
+app.use(prettyJSON());
 
 app.route("/auth", authRouter.getRouter);
 app.get("/getme", async (ctx) => {
