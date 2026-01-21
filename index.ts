d265087e7c6719310e5fc7c8df375932526e7b2e
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2026-01-21 20:13:12 +0800

    Remove environment variable check from server startup

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import app from "./src/server";
 import isEnvsDefined from "./src/utils/checkEnv";
 
-if (!isEnvsDefined()) {
-    console.log(isEnvsDefined());
-    console.error("[ERROR]: Environment variables are not defined.");
-    process.exit(84);
-}
+// if (!isEnvsDefined()) {
+//     console.log(isEnvsDefined());
+//     console.error("[ERROR]: Environment variables are not defined.");
+//     process.exit(84);
+// }
 
 Bun.serve({
     port: parseInt(Bun.env.PORT || "8008"),
